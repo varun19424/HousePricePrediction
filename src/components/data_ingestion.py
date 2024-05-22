8e8f80417a26c69910c353a0402329d4268368f5
varun19424 <72430037+varun19424@users.noreply.github.com>
2024-05-22 23:25:46 +0530

    final

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -49,10 +49,3 @@ class DataIngestion:
             raise CustomException(e,sys)
         
 
-## Run Data Ingestion
-
-if __name__ == '__main__':
-    obj=DataIngestion()
-    train_data_path,test_data_path=obj.initiate_data_ingestion()
-    data_transformation = DataTransformation()
-    train_arr, test_arr,_ = data_transformation.initiate_data_transformation(train_data_path,test_data_path)
